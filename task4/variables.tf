150e4ec4ed654693bb372b0ebaf337c2ed3c3a0f
ShigapovRinat <49624135+ShigapovRinat@users.noreply.github.com>
2025-02-08 01:41:44 +0300

    Create variables.tf

---
+++ task4/variables.tf
@@ -0,0 +1,10 @@
+variable "yc_token" {}
+variable "yc_cloud_id" {}
+variable "yc_folder_id" {}
+variable "yc_zone" {
+  default = "ru-central1-b"
+}
+variable "yc_image_id" {
+  default = "fd8bpal18cm4kprpjc2m" # ID образа Ubuntu 24.04 LTS
+}
+
